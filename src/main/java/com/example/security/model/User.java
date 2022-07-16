e45f406d047aa0294bcf4da60a257302a1ee0b2b
vetterocki <vitbutriy@gmail.com>
2022-07-16 13:27:35 +0300

    Added User Entity

---
+++ src/main/java/com/example/security/model/User.java
@@ -0,0 +1,26 @@
+package com.example.security.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Role role;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+}
