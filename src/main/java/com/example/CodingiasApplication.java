e8e5681ab4348d444412882dac6ab7cf39f460f8
vetterocki <vitbutriy@gmail.com>
2022-07-16 13:27:35 +0300

    Added User Entity

--- src/main/java/com/example/CodingiasApplication.java
+++ src/main/java/com/example/CodingiasApplication.java
@@ -1,15 +1,12 @@
 package com.example;
 
-import com.example.service.JpaGrayCodeTaskLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class CodingiasApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CodingiasApplication.class, args);
+	}
 
-    public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(CodingiasApplication.class, args);
-        ctx.getBean(JpaGrayCodeTaskLoader.class).loadAll();
-    }
 }
---
+++ src/main/java/com/example/security/model/Role.java
@@ -0,0 +1,5 @@
+package com.example.security.model;
+
+public enum Role {
+    SUPER_ADMIN, ADMIN, STUDENT, GUEST
+}
