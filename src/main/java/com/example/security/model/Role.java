2888f3ab8ab1e5df8df91f4ee1a1f85e07a671eb
vetterocki <vitbutriy@gmail.com>
2022-07-21 23:30:20 +0300

    Updated Role And User Entity

--- src/main/java/com/example/security/model/Role.java
+++ src/main/java/com/example/security/model/Role.java
@@ -1,5 +1,39 @@
 package com.example.security.model;
 
-public enum Role {
-    SUPER_ADMIN, ADMIN, STUDENT, GUEST
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Objects;
+
+@Entity
+@Data
+
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id) && Objects.equals(name, role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
