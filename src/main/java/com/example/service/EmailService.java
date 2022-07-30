9033676c6dee12524632fe7c9b782ee1e0c89603
knurff <beryndaviktor@gmail.com>
2022-07-30 21:45:47 +0300

    Comments And Revealed Bug(

---
+++ src/main/java/com/example/service/EmailService.java
@@ -0,0 +1,8 @@
+package com.example.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    //TODO туть можна і хеш функцію якусь інжектнути і одразу розсилку в майбутньому
+}
