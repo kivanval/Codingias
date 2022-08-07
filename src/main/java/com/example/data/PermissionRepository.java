e447fdde6b548644f19fa880aaad734abaf226f0
knurff <beryndaviktor@gmail.com>
2022-08-07 23:20:14 +0300

    Add @Repository to Repositories

--- src/main/java/com/example/data/PermissionRepository.java
+++ src/main/java/com/example/data/PermissionRepository.java
@@ -2,6 +2,8 @@ package com.example.data;
 
 import com.example.model.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PermissionRepository extends JpaRepository<Permission, Long> {
 }
