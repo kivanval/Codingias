243c8d00bbf31cb6679c87ebcd46839a3f1cc66f
knurff <beryndaviktor@gmail.com>
2022-08-07 23:18:48 +0300

    Add SHA-3 Email Hashing Using Guava

--- src/main/java/com/example/GlobalExceptionHandler.java
+++ src/main/java/com/example/GlobalExceptionHandler.java
@@ -2,10 +2,12 @@ package com.example;
 
 import com.example.web.UserAlreadyExistException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ResponseBody
